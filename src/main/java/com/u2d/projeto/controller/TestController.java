5aba8b6b8a30c69d5128313d5eeae4cb2a36ae27
davidjeremias <com1.com3@gmail.com>
2021-03-11 15:45:58 -0300

    correção de versão de erro para teste e change version do deployment

--- src/main/java/com/u2d/projeto/controller/TestController.java
+++ src/main/java/com/u2d/projeto/controller/TestController.java
@@ -1,10 +1,10 @@
 package com.u2d.projeto.controller;
 
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
@@ -13,6 +13,6 @@ public class TestController {
 
     @GetMapping
     public ResponseEntity<?> findMsg(){
-        return ResponseEntity.ok("Teste funcional da api V4");
+        return ResponseEntity.ok("Teste funcional da api V50");
     }
 }
