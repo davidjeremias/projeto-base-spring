3d51f2e46e2677406fcdb1c8b071a6fac56459c7
davidjeremias <com1.com3@gmail.com>
2021-03-10 22:07:36 -0300

    alter msg controller

--- src/main/java/com/u2d/projeto/controller/TestController.java
+++ src/main/java/com/u2d/projeto/controller/TestController.java
@@ -13,6 +13,6 @@ public class TestController {
 
     @GetMapping
     public ResponseEntity<?> findMsg(){
-        return ResponseEntity.ok("Teste funcional da api V2");
+        return ResponseEntity.ok("Teste funcional da api V3");
     }
 }
